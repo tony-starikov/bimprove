6c70b87e967f1b2e602d58511981eb6d5e40c2b7
Tony Starikov <tony.starikov.1992@gmail.com>
2023-05-25 12:14:29 +0300

    Edit favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/x-icon" href="/images/icon.ico">
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
     <title>Main</title>
 {{--    <title>@yield('title')</title>--}}
